7fd999012f03e94d188e19a9374fc0baf670489f
Silasprata23 <silasprata23@gmail.com>
2026-03-08 00:38:14 -0300

    feat:API request

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const pesquisarElement = document.getElementById("pesquisa");
+    const test = document.getElementById("pesquisa").value;
+
+    const key = '3fdb5b8d' ;
+    const query = test;
+
+    // função que ira buscar os filmes ou séris na api
+    async function buscarfilme(){
+        try {
+            const url = await  fetch(`http://www.omdbapi.com/?t=${query}&apikey=${key}`)
+            const dados = await url.json();
+            console.log(dados)
+        }catch (error){
+            console.error("deu ruim" , error)
+        }
+
+    }
+
+    pesquisarElement.addEventListener("input", buscarfilme)
+    buscarfilme();
+
+
+    console.log(query)
+
+
+
+})
